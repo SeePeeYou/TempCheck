42fc5eaedd234718c2645b258368bf6e5a3b7d5e
Tshego Manthata <tshego.manthata@infocarehealth.com>
2024-10-27 20:10:18 +0200

    create navbar component

---
+++ temp-check/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  private router = inject(Router);
+
+  isActive(url: string): boolean {
+    return this.router.url === url;
+  }
+}
